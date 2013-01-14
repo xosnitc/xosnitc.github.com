792f088a9bac8f461ff23016569ebb3384818155
Shamil CM <shamil.cm@gmail.com>
2013-01-14 12:37:00 +0530

    shell modified

--- mastertar.sh
+++ mastertar.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+
 
 mkdir myxos
-wget https://github.com/xosnitc/apl/archive/master.zip .
-wget https://github.com/xosnitc/xosdoc/blob/master/filesystem/filesystem.pdf?raw=true
+mkdir ./myxos/doc
+wget https://github.com/xosnitc/apl/archive/master.zip -P ./myxos
+gunzip ./myxos/master.zip
+mv master apl
+wget https://github.com/xosnitc/xosdoc/blob/master/filesystem/filesystem.pdf?raw=true -P ./myxos/doc/
+mv ./myxos/doc/filesystem.pdf?raw=true ./myxos/doc/filesystem
+wget https://github.com/xosnitc/xosdoc/blob/master/machine/machine.pdf?raw=true -P ./myxos/doc/
+mv ./myxos/doc/machine.pdf?raw=true ./myxos/doc/machine

270835b38744cde3c00f117599cb927279edd596
Shamil CM <shamil.cm@gmail.com>
2013-01-14 15:37:42 +0530

    shell script to create tarball done

--- mastertar.sh
+++ mastertar.sh
@@ -10,13 +10,15 @@ mkdir ./myxos/doc
 wget https://github.com/xosnitc/apl/archive/master.zip -P ./myxos
 
 # Unzip apl pack
-gunzip ./myxos/master.zip
+unzip ./myxos/master.zip -d ./myxos/
+rm ./myxos/master.zip
 
 # Rename master to apl
-mv ./myxos/master ./myxos/apl
+mv ./myxos/apl-master ./myxos/apl
 
 # Remove the .git folder inside apl
 rm -rf ./myxos/apl/.git
+rm -rf ./myxos/apl/.gitignore
 
 # Move APL document to do cfolder
 mv ./myxos/apl/doc/spec.pdf ./myxos/doc/apl.pdf
@@ -27,27 +29,77 @@ rm -rf ./myxos/apl/doc
 wget https://github.com/xosnitc/spl/archive/master.zip -P ./myxos
 
 # Unzip spl pack
-gunzip ./myxos/master.zip
+unzip ./myxos/master.zip -d ./myxos/
+rm ./myxos/master.zip
 
 # Rename master to spl
-mv ./myxos/master ./myxos/spl
+mv ./myxos/spl-master ./myxos/spl
 
 # Remove the .git folder inside spl
 rm -rf ./myxos/spl/.git
+rm -rf ./myxos/spl/.gitignore
 
-# Move spl document to do cfolder
+# Move spl document to doc folder
 mv ./myxos/spl/doc/spec.pdf ./myxos/doc/spl.pdf
 rm -rf ./myxos/spl/doc
 
 
+# XFS INTERFACE
+# Download xfs-interface pack
+wget https://github.com/xosnitc/xfs-interface/archive/master.zip -P ./myxos
 
-#wget https://github.com/xosnitc/xosdoc/blob/master/filesystem/filesystem.pdf?raw=true -P ./myxos/doc/
-#mv ./myxos/doc/filesystem.pdf?raw=true ./myxos/doc/filesystem
-#wget https://github.com/xosnitc/xosdoc/blob/master/machine/machine.pdf?raw=true -P ./myxos/doc/
-#mv ./myxos/doc/machine.pdf?raw=true ./myxos/doc/machine
-#wget https://github.com/xosnitc/xosdoc/blob/master/machine/machine.pdf?raw=true -P ./myxos/doc/
-#mv ./myxos/doc/machine.pdf?raw=true ./myxos/doc/machine
-#wget https://github.com/xosnitc/xosdoc/blob/master/machine/machine.pdf?raw=true -P ./myxos/doc/
-#mv ./myxos/doc/machine.pdf?raw=true ./myxos/doc/machine
-#wget https://github.com/xosnitc/xosdoc/blob/master/machine/machine.pdf?raw=true -P ./myxos/doc/
-#mv ./myxos/doc/machine.pdf?raw=true ./myxos/doc/machine
+# Unzip xfs-interface pack
+unzip ./myxos/master.zip -d ./myxos/
+rm ./myxos/master.zip
+
+# Rename master to xfs-interface
+mv ./myxos/xfs-interface-master ./myxos/xfs-interface
+
+# Remove the .git folder inside xfs-interface
+rm -rf ./myxos/xfs-interface/.git
+rm -rf ./myxos/xfs-interface/.gitignore
+
+# XSM
+
+# Download xsm pack
+wget https://github.com/xosnitc/xsm/archive/master.zip -P ./myxos
+
+# Unzip xsm pack
+unzip ./myxos/master.zip -d ./myxos/
+rm ./myxos/master.zip
+
+# Rename master to xsm
+mv ./myxos/xsm-master ./myxos/xsm
+
+# Remove the .git folder inside xsm
+rm -rf ./myxos/xsm/.git
+rm -rf ./myxos/xsm/.gitignore
+
+
+wget https://github.com/xosnitc/xosdoc/blob/master/filesystem/filesystem.pdf?raw=true -P ./myxos/doc/
+mv ./myxos/doc/filesystem.pdf?raw=true ./myxos/doc/filesystem.pdf
+
+wget https://github.com/xosnitc/xosdoc/blob/master/machine/machine.pdf?raw=true -P ./myxos/doc/
+mv ./myxos/doc/machine.pdf?raw=true ./myxos/doc/machine.pdf
+
+wget https://github.com/xosnitc/xosdoc/blob/master/os/os.pdf?raw=true -P ./myxos/doc/
+mv ./myxos/doc/os.pdf?raw=true ./myxos/doc/os.pdf
+
+wget https://github.com/xosnitc/xosdoc/blob/master/usage/usage.pdf?raw=true -P ./myxos/doc/
+mv ./myxos/doc/usage.pdf?raw=true ./myxos/doc/usage.pdf
+
+
+cp ./files/Makefile ./myxos/
+cp ./files/README ./myxos/
+
+
+nano ./myxos/README
+
+
+tar -cvf myxos.tar.gz ./myxos
+
+mv -f ./myxos.tar.gz ./files/
+
+rm -rf ./myxos
+
+echo "Finished."

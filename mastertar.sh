e8392da334b88b1df517b9845a93fda7a8e81d95
Shamil C M <shamil.cm@gmail.com>
2014-10-20 13:17:03 +0800

    download link corrected

--- mastertar.sh
+++ mastertar.sh
@@ -128,9 +128,6 @@ tar c myxos  | gzip > $filename
 # Move myxos tar
 mv -f $filename ./files/
 
-# Create symbolic link
-ln -s ./files/$filename ./files/myxos.tar.gz
-
 # Remove myxos directory
 rm -rf ./myxos
 
